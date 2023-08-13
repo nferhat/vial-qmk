5625531557b165037d4c39936fb4d0bca3baa81c
nferhat <nferhat20@gmail.com>
2023-08-13 10:04:17 +0100

    Add vial keymap to `gmmk/gmmk2/p65/ansi`

---
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x0D, 0xB2, 0xC7, 0xAF, 0xDC, 0xEB, 0xFF, 0x06}
+
+/* Escape + space + enter */
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 3, 8 }
+#define VIAL_UNLOCK_COMBO_COLS { 3, 1, 4 }
